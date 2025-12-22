54f2ed7b04fd78d85aec7295dd56756a5817d2c0
bala-amypo <bala@amypo.in>
2025-12-22 08:08:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskThresholdRepository.java
+++ src/main/java/com/example/demo/repository/RiskThresholdRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RiskThresholdRepository extends JpaRepository<RiskThreshold, Long> {
-    // required by spec
+    
     Optional<RiskThreshold> findByActiveTrue();
 }
